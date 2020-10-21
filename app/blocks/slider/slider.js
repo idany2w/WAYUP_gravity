3d07691ae6a1cc0412b3d62e485ded45c604e4f0
Daniil Ruzin <jeffgoldtv7@gmail.com>
2020-10-21 18:51:19 +0300

    slider

---
+++ app/blocks/slider/slider.js
@@ -0,0 +1,13 @@
+$('.owl-carousel').owlCarousel({
+    nav: true,
+    margin:30,
+    autoWidth:true,
+    items:1,
+    dots: false,
+    navText: ['',''],
+    navContainerClass: "slider__nav-btns",
+    navClass: [
+        'slider__nav-btn slider__nav-btn_prev',
+        'slider__nav-btn slider__nav-btn_next'
+    ]
+})
