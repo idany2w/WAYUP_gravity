cbc8379da61a1c34e268a63053a4d2ed5f38d8fb
Daniil Ruzin <jeffgoldtv7@gmail.com>
2020-10-21 19:39:31 +0300

    bug fix

--- app/blocks/slider/slider.js
+++ app/blocks/slider/slider.js
@@ -1,13 +1,25 @@
-$('.owl-carousel').owlCarousel({
-    nav: true,
-    margin:30,
-    autoWidth:true,
-    items:1,
-    dots: false,
-    navText: ['',''],
-    navContainerClass: "slider__nav-btns",
-    navClass: [
-        'slider__nav-btn slider__nav-btn_prev',
-        'slider__nav-btn slider__nav-btn_next'
-    ]
+$(window).on('load', function () {
+    $('.owl-carousel').owlCarousel({
+        nav: true,
+        margin:30,
+        items:1,
+        dots: false,
+        navText: ['',''],
+        navContainerClass: "slider__nav-btns",
+        navClass: [
+            'slider__nav-btn slider__nav-btn_prev',
+            'slider__nav-btn slider__nav-btn_next'
+        ],
+        responsive:{
+            576:{
+                autoWidth: false
+            },
+            768:{
+                autoWidth: true
+            },
+        }
+    })
+    $('.slider__slide').click(function(e){
+        alert('click')
+    })
 })
