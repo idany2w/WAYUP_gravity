e163bf9c449b74b12546bbc54f90a5bb499834bb
Daniil Ruzin <jeffgoldtv7@gmail.com>
2020-10-22 20:25:38 +0300

    filters

---
+++ app/blocks/grid-gallery/grid-gallery.js
@@ -0,0 +1,35 @@
+$(window).on('load', function () {
+    $('.grid-gallery__item-btn').click(function(e){
+        alert('click!')
+    });
+})
+
+
+function gridGalleryFilter(e){
+    if(!e.target.classList.contains('grid-gallery__filter-link')) return false
+
+    e.preventDefault();
+
+    const filterBtn = e.target.closest('.grid-gallery__filter-link');
+    const block = filterBtn.closest('.grid-gallery');
+    const filterName = filterBtn.dataset['filter'].toLowerCase();
+    const filterable = block.querySelectorAll('.grid-gallery__filtarable');
+
+    block.querySelector('.grid-gallery__filter-link_active').classList.remove('grid-gallery__filter-link_active');
+    filterBtn.classList.add('grid-gallery__filter-link_active')
+
+    filterable.forEach(element => {
+        const filterNames = element.dataset['filterable'].toLowerCase().split(' ');
+        const height = element.offsetHeight + 'px';
+
+        if(filterNames.indexOf(filterName) != -1){
+            element.classList.remove('grid-gallery__filtarable_hidden');
+        } else{
+            element.style.height = height;
+            element.classList.add('grid-gallery__filtarable_hidden');
+        }
+    });
+
+}
+
+document.addEventListener('click', gridGalleryFilter);
