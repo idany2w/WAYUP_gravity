8e8f290e33c1ab70c9f2e17d5606661093e33e48
Daniil Ruzin <jeffgoldtv7@gmail.com>
2020-10-22 22:58:32 +0300

    clients and footer

---
+++ app/blocks/brands-slider/brands-slider.js
@@ -0,0 +1,9 @@
+$(window).on('load', function () {
+    $('.brands-slider').owlCarousel({
+        nav: false,
+        dots: false,
+        margin:30,
+        items:5,
+        loop: true
+    })
+})
